3e279ec98061e8e445cb0b763e2b31516dc5b367
Connor Slade <connor@connorcode.com>
2022-02-22 19:00:24 -0500

    🎃 Cleanup :P

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
-// use std::fs::File;
-use std::path::Path;
-// use std::fs::OpenOptions;
-use std::fmt::{self, Formatter};
-use std::io::{self, Write};
-use std::io::stdout;
 use core::fmt::Display;
+use std::fmt::{self, Formatter};
+use std::io::stdout;
+use std::io::Write;
+use std::process;
+
 use getch::Getch;
 use rand::seq::SliceRandom;
-use enable_ansi_support;
 
 use crate::parsing::load_into_vec;
-
 mod parsing;
 
 // fancy ANSI formatting
 enum ANSI {
     Bold,
     Fore256(u8),
-    Back256(u8), 
+    Back256(u8),
     MoveCursor(i32, i32),
     Back,
     Clear,
@@ -34,52 +31,38 @@ enum Loc {
 
 enum Preset {
     Green,
-    Yellow
+    Yellow,
 }
 
 impl Display for ANSI {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            ANSI::Bold                  => f.write_str("\x1b[1m"),
-            ANSI::Fore256(id)           => write!(f, "\x1b[38;5;{}m", id),
-            ANSI::Back256(id)           => write!(f, "\x1b[48;5;{}m", id),
-            ANSI::MoveCursor(y, x)      => write!(f, "\x1b[{};{}H", y, x),
-            ANSI::Back                  => write!(f, "\x1b[1D"),
-            ANSI::Clear                 => write!(f, "\x1b[0m"),
-            ANSI::ClearScreen           => write!(f, "\x1b[2J"),
+            ANSI::Bold => f.write_str("\x1b[1m"),
+            ANSI::Fore256(id) => write!(f, "\x1b[38;5;{}m", id),
+            ANSI::Back256(id) => write!(f, "\x1b[48;5;{}m", id),
+            ANSI::MoveCursor(y, x) => write!(f, "\x1b[{};{}H", y, x),
+            ANSI::Back => write!(f, "\x1b[1D"),
+            ANSI::Clear => write!(f, "\x1b[0m"),
+            ANSI::ClearScreen => write!(f, "\x1b[2J"),
 
             ANSI::Move(location) => {
                 // hardcoded :lol:
                 let pos: (i32, i32) = match location {
-                    Loc::Home       => (0, 0),          // (0,0), the home position
-                    Loc::Start      => (10, 0),         // after the title
-                    Loc::Word(n)    => (11 + n, 5),     // go to the start of a specific word
-                    Loc::Bottom     => (17, 2),
+                    Loc::Home => (0, 0),         // (0,0), the home position
+                    Loc::Start => (10, 0),       // after the title
+                    Loc::Word(n) => (11 + n, 5), // go to the start of a specific word
+                    Loc::Bottom => (17, 2),
                 };
                 let (y, x) = pos;
                 write!(f, "\x1b[{};{}H", y, x)
-            },
-            ANSI::Preset(preset) => {
-                match preset {
-                    Preset::Green   => write!(f, "{}{}", ANSI::Back256(2), ANSI::Fore256(0)),
-                    Preset::Yellow  => write!(f, "{}{}", ANSI::Back256(3), ANSI::Fore256(0)),
-                }
             }
+            ANSI::Preset(preset) => match preset {
+                Preset::Green => write!(f, "{}{}", ANSI::Back256(2), ANSI::Fore256(0)),
+                Preset::Yellow => write!(f, "{}{}", ANSI::Back256(3), ANSI::Fore256(0)),
+            },
         }
     }
 }
-
-// enum State {
-//     Input(char),
-//     Restart,
-//     TooShort,
-//     waiting,
-// }
-// impl PartialEq for State {
-//     fn eq(&self, other: &Self) -> bool {
-//         self == other
-//     }
-// }
 
 fn get_word() -> String {
     let ch: Getch = Getch::new();
@@ -88,21 +71,24 @@ fn get_word() -> String {
     while len < 11 {
         let key = ch.getch().unwrap();
         match key {
-            27 => panic!("pressed ESC!"),
+            27 => {
+                println!("{}{}Pressed ESC", ANSI::ClearScreen, ANSI::Move(Loc::Home));
+                process::exit(0);
+            }
             97..=122 => {
                 print!("{}", (key - 32) as char);
-                stdout().flush();
+                stdout().flush().unwrap();
                 out[len] = key as char;
                 len += 1;
-            },
-            8  => {
+            }
+            8 => {
                 if len > 0 {
                     len -= 1;
                     print!("{}*{}", ANSI::Back, ANSI::Back);
-                    stdout().flush();
+                    stdout().flush().unwrap();
                 }
-            },
-            10 | 13 => return "jank workaround lol dont mind me".to_string(),
+            }
+            10 | 13 => return "jank workaround lol dont mind me".to_owned(),
             _ => continue,
         }
     }
@@ -113,30 +99,34 @@ fn get_word() -> String {
     ret
 }
 
-
 fn main() {
-    enable_ansi_support::enable_ansi_support();
+    enable_ansi_support::enable_ansi_support().unwrap();
 
-    print!("{}{}{}
+    print!(
+        "{}{}{}
 {}      :::       :::  ::::::::::  :::::::::   :::::::::   :::         ::::::::::{} (c)(r)(tm) {}
-{}     :+:       :+:  :+:         :+:    :+:  :+:    :+:  :+:         :+:        
-{}    +:+       +:+  +:+         +:+    +:+  +:+    +:+  +:+         +:+         
-{}   +#+  +:+  +#+  +#++:++#    +#++:++#:   +#+    +:+  +#+         +#++:++#     
-{}  +#+ +#+#+ +#+  +#+         +#+    +#+  +#+    +#+  +#+         +#+           
-{} #+#+# #+#+#    #+#         #+#    #+#  #+#    #+#  #+#         #+#             
+{}     :+:       :+:  :+:         :+:    :+:  :+:    :+:  :+:         :+:
+{}    +:+       +:+  +:+         +:+    +:+  +:+    +:+  +:+         +:+
+{}   +#+  +:+  +#+  +#++:++#    +#++:++#:   +#+    +:+  +#+         +#++:++#
+{}  +#+ +#+#+ +#+  +#+         +#+    +#+  +#+    +#+  +#+         +#+
+{} #+#+# #+#+#    #+#         #+#    #+#  #+#    #+#  #+#         #+#
 {}###   ###      ##########  ###    ###  #########   ##########  ##########{}
 ──────────────────────────────────────────────────────────────────────────────────────────────
-",     
-    ANSI::Move(Loc::Home), ANSI::ClearScreen, ANSI::Bold,
-    ANSI::Fore256(196),
-    ANSI::Clear, ANSI::Bold,
-    ANSI::Fore256(202),
-    ANSI::Fore256(208),
-    ANSI::Fore256(208),
-    ANSI::Fore256(214),
-    ANSI::Fore256(214),
-    ANSI::Fore256(220),
-    ANSI::Clear);
+",
+        ANSI::Move(Loc::Home),
+        ANSI::ClearScreen,
+        ANSI::Bold,
+        ANSI::Fore256(196),
+        ANSI::Clear,
+        ANSI::Bold,
+        ANSI::Fore256(202),
+        ANSI::Fore256(208),
+        ANSI::Fore256(208),
+        ANSI::Fore256(214),
+        ANSI::Fore256(214),
+        ANSI::Fore256(220),
+        ANSI::Clear
+    );
 
     let p_accepted_words = include_str!("../data/alphabetized/all/11.txt");
     let p_possible_ans = include_str!("../data/alphabetized/filtered/11.txt");
@@ -144,57 +134,71 @@ fn main() {
     let accepted_words: Vec<String> = load_into_vec(p_accepted_words);
     let possible_ans: Vec<String> = load_into_vec(p_possible_ans);
 
-    'outer: loop {
-        let answer: String = possible_ans.choose(&mut rand::thread_rng()).unwrap().to_string();
+    loop {
+        let answer: String = possible_ans
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .to_owned();
         let c_freq: [u8; 27] = {
-            let mut out: [u8; 27] = [0;27];
+            let mut out: [u8; 27] = [0; 27];
             for item in answer.chars() {
                 out[item as usize - 'a' as usize] += 1;
             }
             out
         };
-        // println!("{:?}", freq);
 
-        print!("{}\n    \
+        print!(
+            "{}\n    \
     ***********       Press Enter to give up
     ***********
     ***********        Q W E R T Y U I O P
     ***********         A S D F G H J K L
     ***********          Z X C V B N M <- ignore this for now thanks
     ***********                            (ill add it in later)
-", ANSI::Move(Loc::Start));
+",
+            ANSI::Move(Loc::Start)
+        );
         let mut itr = 0;
-        'words: while itr < 6 {
+        while itr < 6 {
             print!("{}", ANSI::Move(Loc::Word(itr)));
-            stdout().flush();
-            let mut attempt: String = get_word();
+            stdout().flush().unwrap();
+            let attempt: String = get_word();
             // oh yay we found it
-            // println!("{:#?}", answer);
-            
-            if attempt == "jank workaround lol dont mind me".to_string() { break; }
-            match accepted_words.binary_search(&attempt) { 
-                Err(_) => {
-                    print!("{}***********", ANSI::Move(Loc::Word(itr))); 
-                    continue; 
-                },
-                _ => {},
+
+            if attempt == *"jank workaround lol dont mind me" {
+                break;
             }
-            //println!("{:#?}", accepted_words.binary_search(&attempt));
-            print!("{}", ANSI::Move(Loc::Word(itr)));            
+
+            if accepted_words.binary_search(&attempt).is_err() {
+                print!("{}***********", ANSI::Move(Loc::Word(itr)));
+                continue;
+            }
+
+            print!("{}", ANSI::Move(Loc::Word(itr)));
             // mark the letters!
-            let mut freq: [u8; 27] = c_freq.clone();
+            let mut freq: [u8; 27] = c_freq;
             for (i, letter) in attempt.chars().enumerate() {
                 if letter as u8 == answer.as_bytes()[i] {
-                    print!("{}{}", ANSI::Preset(Preset::Green), (letter as u8 - 32) as char);
+                    print!(
+                        "{}{}",
+                        ANSI::Preset(Preset::Green),
+                        (letter as u8 - 32) as char
+                    );
                     freq[letter as usize - 'a' as usize] -= 1;
                     continue;
                 }
                 print!("{}{}", ANSI::Clear, (letter as u8 - 32) as char);
             }
+
             print!("{}", ANSI::Move(Loc::Word(itr)));
             for (i, letter) in attempt.chars().enumerate() {
-                if freq[letter as usize - 'a' as usize] > 0 && letter as u8 != answer.as_bytes()[i] {
-                    print!("{}{}", ANSI::Preset(Preset::Yellow), (letter as u8 - 32) as char);
+                if freq[letter as usize - 'a' as usize] > 0 && letter as u8 != answer.as_bytes()[i]
+                {
+                    print!(
+                        "{}{}",
+                        ANSI::Preset(Preset::Yellow),
+                        (letter as u8 - 32) as char
+                    );
                     freq[letter as usize - 'a' as usize] -= 1;
                     continue;
                 }
@@ -202,9 +206,19 @@ fn main() {
             }
             itr += 1;
             print!("{}", ANSI::Clear);
-            if attempt == answer { break; }
+            if attempt == answer {
+                break;
+            }
         }
-        print!("{}{}The word was: {}\npress Enter to continue\n{}", ANSI::Fore256(3), ANSI::Move(Loc::Bottom), answer, ANSI::Clear);
+
+        print!(
+            "{}{}The word was: {}\npress Enter to continue\n{}",
+            ANSI::Fore256(3),
+            ANSI::Move(Loc::Bottom),
+            answer,
+            ANSI::Clear
+        );
+
         let ch: Getch = Getch::new();
         while ch.getch().unwrap() != 13 {}
         print!("\r\x1b[2A\x1b[0J")
